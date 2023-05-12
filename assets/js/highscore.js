ad6506350f92ab80a5fc5fbb0900d49a31aa0c0f
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-05-12 19:05:14 +0200

    add highscore modal

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,23 +1,22 @@
 const highscoreData = [
-    { name: 'John', score: 100 },
-    { name: 'Mary', score: 90 },
-    { name: 'Tom', score: 80 },
-  ];
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const highscoreBtn = document.getElementById('highscore-btn');
-    const highscoreList = document.getElementById('highscore-list');
-  
-    highscoreBtn.addEventListener('click', () => {
-      // Clear any previous highscore data
-      highscoreList.innerHTML = '';
-  
-      // Loop through the highscore data and create list items
-      for (const { name, score } of highscoreData) {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${name}: ${score}`;
-        highscoreList.appendChild(listItem);
-      }
-    });
+  { name: 'Luke', score: 300 },
+  { name: 'Leia', score: 200 },
+  { name: 'Han', score: 100 }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const highscoreBtn = document.getElementById('highscore-btn');
+  const highscoreList = document.getElementById('highscore-list');
+
+  highscoreBtn.addEventListener('click', () => {
+    // Clear any previous highscore data
+    highscoreList.innerHTML = '';
+
+    // Loop through the highscore data and create list items
+    for (const { name, score } of highscoreData) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${name}: ${score}`;
+      highscoreList.appendChild(listItem);
+    }
   });
-  
+});
