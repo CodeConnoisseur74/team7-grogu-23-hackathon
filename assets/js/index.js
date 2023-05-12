dfb4b34aaf1614191864bf79c0f6a4c7b26dd05d
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-05-12 17:52:08 +0200

    fix conflict

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
 "use strict";
 
 // Main fil for index page.
+
+
+// Audio sound press button
+const buttons = document.querySelectorAll('.colour-btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = new Audio(`assets/audio/${button.dataset.sound}`);
+    sound.play();
+  });
+});
