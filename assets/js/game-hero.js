48a6bfac553a6304ff2e77fe34a2550627a27594
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-05-13 18:10:23 +0100

    Added Dice functions

--- assets/js/game-hero.js
+++ assets/js/game-hero.js
@@ -1,191 +1,187 @@
 "use strict";
 
-
 //HERO OBJECTS
 //Declare the current hero object
-let currentGameHeroData = {}
+let currentGameHeroData = {};
 
 //Declare hero data objects that is accessed when generating
 // the data for the selected hero.
 let heroesData = [
-    {
-        name: "Grogu",
-        diceAmount: {
-            red: 3,
-            blue: 3,
-            green: 3,
-            yellow: 3,
-            black: 0,
-        },
-        diceLimit: {
-            red: 6,
-            blue: 6,
-            green: 6,
-            yellow: 6,
-            black: 6,
-        },
-        maxLife: 30,
-        currentLife: 20,
-        image: "assets/images/grogu.png",
-        description: "", 
+  {
+    name: "Grogu",
+    diceAmount: {
+      red: 3,
+      blue: 3,
+      green: 3,
+      yellow: 3,
+      black: 0,
     },
-    {
-        name: "Mandalorian",
-        diceAmount: {
-            red: 5,
-            blue: 3,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        },
-        diceLimit: {
-            red: 6,
-            blue: 6,
-            green: 6,
-            yellow: 6,
-            black: 6,
-        },
-        maxLife: 30,
-        currentLife: 30,
-        image: "assets/images/mandalorian.png",
-        description: "", 
+    diceLimit: {
+      red: 6,
+      blue: 6,
+      green: 6,
+      yellow: 6,
+      black: 6,
     },
-    {
-        name: "R2-D2",
-        diceAmount: {
-            red: 1,
-            blue: 3,
-            green: 5,
-            yellow: 2,
-            black: 1,
-        },
-        diceLimit: {
-            red: 6,
-            blue: 6,
-            green: 6,
-            yellow: 6,
-            black: 6,
-        },
-        maxLife: 30,
-        currentLife: 30,
-        image: "assets/images/r2-d2.png",
-        description: "", 
+    maxLife: 30,
+    currentLife: 20,
+    image: "assets/images/grogu.png",
+    description: "",
+  },
+  {
+    name: "Mandalorian",
+    diceAmount: {
+      red: 5,
+      blue: 3,
+      green: 2,
+      yellow: 2,
+      black: 0,
     },
-    {
-        name: "Rey",
-        diceAmount: {
-            red: "3",
-            blue: "1",
-            green: "2",
-            yellow: "5",
-            black: "1",
-        },
-        diceLimit: {
-            red: 6,
-            blue: 6,
-            green: 6,
-            yellow: 6,
-            black: 6,
-        },
-        maxLife: 30,
-        currentLife: 30,
-        image: "assets/images/rey.png",
-        description: "", 
+    diceLimit: {
+      red: 6,
+      blue: 6,
+      green: 6,
+      yellow: 6,
+      black: 6,
     },
+    maxLife: 30,
+    currentLife: 30,
+    image: "assets/images/mandalorian.png",
+    description: "",
+  },
+  {
+    name: "R2-D2",
+    diceAmount: {
+      red: 1,
+      blue: 3,
+      green: 5,
+      yellow: 2,
+      black: 1,
+    },
+    diceLimit: {
+      red: 6,
+      blue: 6,
+      green: 6,
+      yellow: 6,
+      black: 6,
+    },
+    maxLife: 30,
+    currentLife: 30,
+    image: "assets/images/r2-d2.png",
+    description: "",
+  },
+  {
+    name: "Rey",
+    diceAmount: {
+      red: "3",
+      blue: "1",
+      green: "2",
+      yellow: "5",
+      black: "1",
+    },
+    diceLimit: {
+      red: 6,
+      blue: 6,
+      green: 6,
+      yellow: 6,
+      black: 6,
+    },
+    maxLife: 30,
+    currentLife: 30,
+    image: "assets/images/rey.png",
+    description: "",
+  },
 ];
 
 //SELECT HERO FUNCTIONALITIES
 
 //When html is dowloaded the modal HTML is dowloaded using the heroesData object
-renderModalHeroes()
+renderModalHeroes();
 
-function renderModalHeroes(){
-    const modalContent = document.getElementById("modal-heroes")
-    let modalInner = ""
-    for(let i in heroesData){
-        modalInner += `
-        <div class="carousel-item ${(i==0) ? "active":"not-active"} selectHero" id="${heroesData[i].name}">
+function renderModalHeroes() {
+  const modalContent = document.getElementById("modal-heroes");
+  let modalInner = "";
+  for (let i in heroesData) {
+    modalInner += `
+        <div class="carousel-item ${i == 0 ? "active" : "not-active"} selectHero" id="${heroesData[i].name}">
             <img src="${heroesData[i].image}" class="d-block mx-auto" alt="Image of ${heroesData[i].name}">
             <p class="mx-auto d-block">${heroesData[i].name}<br>${heroesData[i].description}</p>
-        </div>` 
-    }
-    console.log(modalInner)
-    modalContent.innerHTML = modalInner
+        </div>`;
+  }
+  console.log(modalInner);
+  modalContent.innerHTML = modalInner;
 }
 
 //The eventlistener for the modals carousle-items belonign to selectHero class
 //Calls renderHeroGameProfile
-document.querySelectorAll(".selectHero").forEach(occurence => {
-    let id = occurence.getAttribute('id');
-    occurence.addEventListener('click', function() {
-        renderHeroGameProfile(id)
-    } );
+document.querySelectorAll(".selectHero").forEach((occurence) => {
+  let id = occurence.getAttribute("id");
+  occurence.addEventListener("click", function () {
+    renderHeroGameProfile(id);
+  });
 });
 
 //UPDATE HERO DATA
 //This function initiates the hero profile update
-function renderHeroGameProfile(hero){
-    let heroObject = {}
-    for (let i of heroesData){
-        if (i.name == hero){
-            heroObject = i;
-        };
-    };
-    currentGameHeroData = heroObject;
-    renderHeroPowers()
-    renderHeroLife()
-    renderHeroImg()
-    console.log(currentGameHeroData);
+function renderHeroGameProfile(hero) {
+  let heroObject = {};
+  for (let i of heroesData) {
+    if (i.name == hero) {
+      heroObject = i;
+    }
+  }
+  currentGameHeroData = heroObject;
+  renderHeroPowers();
+  renderHeroLife();
+  renderHeroImg();
 }
 
 //Function that renders power stats
-function renderHeroPowers(){
-    const redPower = currentGameHeroData.diceAmount.red;
-    const bluePower = currentGameHeroData.diceAmount.blue;
-    const greenPower = currentGameHeroData.diceAmount.green;
-    const yellowPower = currentGameHeroData.diceAmount.yellow;
-    const blackPower = currentGameHeroData.diceAmount.black;
+function renderHeroPowers() {
+  const redPower = currentGameHeroData.diceAmount.red;
+  const bluePower = currentGameHeroData.diceAmount.blue;
+  const greenPower = currentGameHeroData.diceAmount.green;
+  const yellowPower = currentGameHeroData.diceAmount.yellow;
+  const blackPower = currentGameHeroData.diceAmount.black;
 
-    const redLimit = currentGameHeroData.diceLimit.red;
-    const blueLimit = currentGameHeroData.diceLimit.blue;
-    const greenLimit = currentGameHeroData.diceLimit.green;
-    const yellowLimit = currentGameHeroData.diceLimit.yellow;
-    const blackLimit = currentGameHeroData.diceLimit.black;
+  const redLimit = currentGameHeroData.diceLimit.red;
+  const blueLimit = currentGameHeroData.diceLimit.blue;
+  const greenLimit = currentGameHeroData.diceLimit.green;
+  const yellowLimit = currentGameHeroData.diceLimit.yellow;
+  const blackLimit = currentGameHeroData.diceLimit.black;
 
-    const diceSlots = document.getElementsByClassName("power-badges")
-    diceSlots[0].innerHTML = `${redPower}/${redLimit}`;
-    diceSlots[1].innerHTML = `${bluePower}/${blueLimit}`;
-    diceSlots[2].innerHTML = `${greenPower}/${greenLimit}`;
-    diceSlots[3].innerHTML = `${yellowPower}/${yellowLimit}`;
-    diceSlots[4].innerHTML = `${blackPower}/${blackLimit}`;
+  const diceSlots = document.getElementsByClassName("power-badges");
+  diceSlots[0].innerHTML = `${redPower}/${redLimit}`;
+  diceSlots[1].innerHTML = `${bluePower}/${blueLimit}`;
+  diceSlots[2].innerHTML = `${greenPower}/${greenLimit}`;
+  diceSlots[3].innerHTML = `${yellowPower}/${yellowLimit}`;
+  diceSlots[4].innerHTML = `${blackPower}/${blackLimit}`;
 }
 
 //Function that renders hero life
-function renderHeroLife(){
-    const maxLife = currentGameHeroData.maxLife;
-    const currentLife = currentGameHeroData.currentLife;
-    const healthRemaining = document.getElementById("healthRemaining");
-    const healthPoints = document.getElementById("health-points");
+function renderHeroLife() {
+  const maxLife = currentGameHeroData.maxLife;
+  const currentLife = currentGameHeroData.currentLife;
+  const healthRemaining = document.getElementById("healthRemaining");
+  const healthPoints = document.getElementById("health-points");
 
-    healthRemaining.style.width = `${100*currentLife/maxLife}%`
-    healthPoints.innerHTML = `${currentLife}/${maxLife}`
+  healthRemaining.style.width = `${(100 * currentLife) / maxLife}%`;
+  healthPoints.innerHTML = `${currentLife}/${maxLife}`;
 }
 
 //Function that renders the hero photo
-function renderHeroImg(){
-    const gameHeroImg = document.getElementById("game-hero-img");
-    gameHeroImg.setAttribute("src", currentGameHeroData.image);
+function renderHeroImg() {
+  const gameHeroImg = document.getElementById("game-hero-img");
+  gameHeroImg.setAttribute("src", currentGameHeroData.image);
 }
 
 //Function updates the player stats according to the selected rewards
-function addRewardsToHero(selectedReward){
-}
+function addRewardsToHero(selectedReward) {}
 
 //Clears the hero data
-function clearHeroProfile(){
-    currentGameHeroData = {};
+function clearHeroProfile() {
+  currentGameHeroData = {};
 }
-
 
 //Used when the current game reloaded or when round is over, and hero loses helth
 //Updates current game hero profile stats
--- assets/js/game-setup.js
+++ assets/js/game-setup.js
@@ -4,48 +4,39 @@
 
 // Starting game settings
 const gameSettings = {
-    score: 0,
-    level: 0,
-    diceArrangment: size
-}
-
+  score: 0,
+  level: 0,
+  diceId: 1,
+  diceArrangment: "size",
+};
 
 // Loaded game settings
-const currentGameSettings = {
-    
-}
-
+const currentGameSettings = {};
 
 // Is used to find out random number
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-
 window.addEventListener("load", () => {
-    // Code to be added later
-})
-
+  // Code to be added later
+});
 
 function endRound() {
-    calculateHealth() // Not done yet
-    calculateScoreGained() // Not done yet
-    renderGameScore() // Not done yet
-    addGameLevel()
-    clearVillainProfile()
-    updateHeroGameProfile() // Not done yet
-    renderHeroStats() // Not done yet
+  calculateHealth(); // Not done yet
+  calculateScoreGained(); // Not done yet
+  renderGameScore(); // Not done yet
+  addGameLevel();
+  clearVillainProfile();
+  updateHeroGameProfile(); // Not done yet
+  renderHeroStats(); // Not done yet
 }
 
-
-function addGameLevel(){
-    currentGameSettings.level++
+function addGameLevel() {
+  currentGameSettings.level++;
 }
 
-
-function clearVillainProfile(){
-    const villianImage= document.getElementById("villian-image")
-    const villianDescription= document.getElementById("villian-description")
-    villianImage.innerHTML= ""
-    villianDescription.innerHTML= ""
+function clearVillainProfile() {
+  const villianImage = document.getElementById("villian-image");
+  const villianDescription = document.getElementById("villian-description");
+  villianImage.innerHTML = "";
+  villianDescription.innerHTML = "";
 }
-
-
